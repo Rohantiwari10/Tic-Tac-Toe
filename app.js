83314b79721a24f5d5fc9c7ec7f19068ca96b4c1
Rohan Tiwari <rohanitsector@gmail.com>
2025-03-21 21:01:45 +0530

    added reset and new game button

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ const winPatterns = [
     [6,7,8],
 ];
 
+const resetGame = () =>{
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
 boxes.forEach((box) => {
     box.addEventListener("click",() => {
         console.log("Box was clicked");
@@ -32,9 +38,23 @@ boxes.forEach((box) => {
     })
 });
 
+const disableBoxes = ()=> {
+     for(let box of boxes){
+        box.disabled = true;
+     }
+}
+
+const enableBoxes = ()=> {
+    for(let box of boxes){
+       box.disabled = false;
+       box.innerText = "";
+    }
+}
+
 const showWinner = (winner) => {
      msg.innerText = `Congratulations, Winner is ${winner}`;
      msgContainer.classList.remove("hide");
+     disableBoxes();
 }
 
 const checkWinner = () => {
@@ -51,3 +71,6 @@ const checkWinner = () => {
         }
     }
 }
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
