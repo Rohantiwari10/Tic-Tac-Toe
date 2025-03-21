69597a250cb2bb138a7e4d79635527d0e8ba16db
Rohan Tiwari <rohanitsector@gmail.com>
2025-03-21 20:46:54 +0530

    added winner functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turn0 = true;  //player X, Player 0
 
@@ -25,7 +28,26 @@ boxes.forEach((box) => {
             turn0 = true;
         }
         box.disabled = true;
-        
+        checkWinner();
     })
 });
 
+const showWinner = (winner) => {
+     msg.innerText = `Congratulations, Winner is ${winner}`;
+     msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("Winner",pos1Val); 
+                showWinner(pos1Val); 
+            }
+        }
+    }
+}
